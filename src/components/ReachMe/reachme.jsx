21c4a39b884ea829f861d1970faedb27fe730b10
AhamedHarish <ahamed.harish05@gmail.com>
2025-05-12 23:11:19 +0530

    EmailJs Changes

--- src/components/ReachMe/reachme.jsx
+++ src/components/ReachMe/reachme.jsx
@@ -13,18 +13,20 @@ const Reachme = () => {
     e.preventDefault();
 
     emailjs
-      .sendForm("service_bqb9q5l", "template_xum1ofm", form.current, {
-        publicKey: "t0X8WIUezsy3SZwDi",
+      .sendForm("service_wytsm9m", "template_7ac21j4", form.current, {
+        publicKey: "V7_904iijo0H6k3Du",
       })
       .then(
         () => {
           console.log("SUCCESS!");
 
           // Reset the form after successful submission
+          alert("Message sent successfully!");
           form.current.reset();
         },
         (error) => {
           console.log("FAILED...", error.text);
+          alert("Failed to send message, Please try again!",error.text);
         }
       );
   };
@@ -39,7 +41,7 @@ const Reachme = () => {
         <button className='reachMeBtn' type="submit" value="send">Submit</button>
      </form>
      <div className='socialLink'>
-            <a href='www.linkedin.com/in/ahamed-harish-othman-5ba25b21b' target='_blank'><img className='linkedIn' src={LinkedIn} alt='linkedin logo'/></a>
+            <a href='https://www.linkedin.com/in/ahamed-harish-othman-5ba25b21b' target='_blank' rel='noopener noreferrer'><img className='linkedIn' src={LinkedIn} alt='linkedin logo'/></a>
             <a href='https://github.com/AhamedHarish' target="_blank"><img className='git' src={Git} alt='github logo'/></a>
             <a href='https://www.instagram.com/ah.harishhh?igsh=MxJpYmkzZHM5NTBjZA==' target="_blank"><img className='insta' src={IG} alt='instagram logo'/></a>
      </div>
